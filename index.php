805d2e77977e10a0f2c7d056cdd56a583a39ac0d
Tantri <ltantri71@gmail.com>
2025-06-24 21:41:39 +0700

    Meenambahkan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote lagi dengan folder berbeda yaitu git remote 2</title>
+</head>
+<body>
+    <h1>Belajar git remote yang ke2</h1>
+</body>
+</html>
